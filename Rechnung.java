1bc9d9cc43186e71f136a7d9ecb6900353231d90
Darktobi <tobi_360@yahoo.de>
2016-06-01 13:47:55 +0200

    added attributes for artikel and methods for Rechnung

---
+++ Rechnung.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+public class Rechnung{
+	
+	ArrayList<Artikel> artList = new ArrayList<Artikel>();
+	
+	public void add(Artikel art){
+		artList.add(art);
+	}
+	
+	public void drucken(){
+	
+	}
+
+}
